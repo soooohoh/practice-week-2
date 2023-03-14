257c43a1935511d344c8c35360ce08f9a5b65fb6
soooohoh <tngh8597@naver.com>
2023-03-14 14:28:41 +0900

    TC-4 create multiple function

--- main.c
+++ main.c
@@ -12,26 +12,31 @@ int multi(int x, int y){
     return x*y;
 }
 
-int div(int x, int y){
-    return x/y;
-}
-
 int main(){
     int x, y, op;
 
     printf("Enter two numbers(x,y) : ");
     scanf("%d %d",&x ,&y);
 
-    printf("\nEnter the operation\n1.plus\n2.minus\n3.multiple\n4.division\n>>");
+    printf("\nEnter the operation\n1.plus\n2.minus\n3.multiple\n>>");
     scanf("%d",&op);
 
     int res;
 
-
-    if(op==1) res = plus(x,y);
-    if(op==2) res = minus(x,y);
-    if(op==3) res = multi(x,y);
-    if(op==4) res = div(x,y);    
+    switch (op)
+    {
+    case 1:
+        res = plus(x,y);       
+        break;
+    case 2:
+        res = minus(x,y);
+        break;
+    case 3:
+        res = multi(x,y);
+        break;
+    default:
+        break;
+    }
 
 
     printf("\nResult is [%d]\n",res);
